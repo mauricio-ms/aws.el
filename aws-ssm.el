45336ea452d10a331719ac576de67c471280124a
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2023-08-10 22:01:41 -0300

    make to load all parameters and add serialization for caching values in the filesystem

--- aws-ssm.el
+++ aws-ssm.el
@@ -19,55 +19,84 @@
 (defconst aws-ssm--value-limit-length 80
   "Constant to define the limit length that values displayed in the aws-ssm list can have. values with a value larger than this limit should be truncated.")
 
-(defvar aws-ssm--parameters-model nil
-  "Cache to store the parameters candidates model")
-
-(defvar aws-ssm--parameters-view nil
-  "Cache to store the parameters candidates view")
-
 ;; TODO => Add comments
-;; TODO => Remove max-items parameter
 ;; TODO => Serialize and deserialize the cache variables
 
 (defun aws-ssm ()
   (interactive)
-  (consult--read aws-ssm--parameters-view
-				 :prompt "SSM: "
-				 :lookup (lambda (selected candidates input narrow)
-						   (let ((value (gethash (s-trim (nth 0 (s-split "=>" selected)))
-												 aws-ssm--parameters-model)))
-							 (if (aws-ssm--truncate? value aws-ssm--value-limit-length)
-								 (let ((buffer (get-buffer-create "*aws-ssm*")))
-								   (with-current-buffer buffer
-									 (erase-buffer)
-									 (insert value)
-									 (switch-to-buffer buffer)))
-							   (message value))))))
+  (let ((parameters-model (aws-ssm--read-data "data.txt")))
+	(consult--read (aws-ssm--get-parameters-view parameters-model)
+				   :prompt "SSM: "
+				   :lookup (lambda (selected candidates input narrow)
+							 (let ((value (gethash (s-trim (nth 0 (s-split "=>" selected)))
+												   parameters-model)))
+							   (if (aws-ssm--truncate? value aws-ssm--value-limit-length)
+								   (let ((buffer (get-buffer-create "*aws-ssm*")))
+									 (with-current-buffer buffer
+									   (erase-buffer)
+									   (insert value)
+									   (switch-to-buffer buffer)
+									   (forward-line 1)))
+								 (message value)))))))
 
-(defun aws-ssm-reload-cache ()
+(defun aws-ssm--get-parameters-view (parameters-model)
+  (let ((parameters-view))
+	(maphash
+	 (lambda (k v)
+	   (push
+		(format "%-80s => %s"
+				k
+				(propertize (aws-ssm--truncate v aws-ssm--value-limit-length)
+							'face 'aws-ssm-value-font))
+		parameters-view))
+	 parameters-model)
+	parameters-view))
+
+;; load cache
+(defun aws-ssm-update-cache ()
   (interactive)
-  (message "AWS SSM updating parameters cache ...")
+  (message "AWS SSM updating cache ...")
   (async-start
-   (aws-ssm--get-parameters-names-async "aws ssm describe-parameters --max-items 10")
-   (lambda (names)
-	 (let ((get-parameters-command (format "aws ssm get-parameters --names %s" (string-join names " "))))
-	   (async-start
-		(aws-ssm--get-parameters-values-async get-parameters-command)
-		(lambda (values)
-		  (let ((ht (make-hash-table :test 'equal)))
-			(seq-do (lambda (elt)
-					  (puthash (nth 0 elt) (nth 1 elt) ht))
-					values)
-			(setq aws-ssm--parameters-model ht))
-		  (setq aws-ssm--parameters-view (mapcar
-										  (lambda (elt)
-											(format "%-80s => %s"
-													(nth 0 elt)
-													(propertize (aws-ssm--truncate (nth 1 elt) aws-ssm--value-limit-length)
-																'face 'aws-ssm-value-font)))
-										  values))
-		  (message "AWS SSM parameters cache updated")))))))
+   (aws-ssm--get-parameters-async)
+   (lambda (parameters)
+	 (let ((ht (make-hash-table :test 'equal)))
+	   (seq-do (lambda (elt)
+				 (puthash (nth 0 elt) (nth 1 elt) ht))
+			   parameters)
+	   (aws-ssm--write-data "data.txt" ht))
+	 
+	 (message "AWS SSM cache updated"))))
 
+(defun aws-ssm--get-parameters-async ()
+  (lambda ()
+	(defun execute-command ()
+	  (json-parse-string (shell-command-to-string "aws ssm get-parameters-by-path --path / --recursive --query \"Parameters[*].{Name:Name,Value:Value}\"")))
+
+	(defun parse-get-parameters-resp (command-result-json)
+	  (mapcar (lambda (x)
+				`(,(gethash "Name" x)
+				  ,(gethash "Value" x)))
+			  command-result-json))
+	
+	(parse-get-parameters-resp (execute-command))))
+
+
+;; (benchmark-elapse
+;;   (let ((prefix-command "aws ssm get-parameters-by-path --path / --recursive --page-size 10 --query \"{Parameters:Parameters[*].{Name:Name,Value:Value},NextToken:NextToken}\"")
+;; 		(has-next t)
+;; 		(next-token-param)
+;; 		(l 0))
+;; 	(while has-next
+;; 	  (let* ((command (format "%s%s" prefix-command (or next-token-param "")))
+;; 			 (command-response (shell-command-to-string command)))
+;; 		(let* ((result (json-parse-string command-response))
+;; 			   (next-token (gethash "NextToken" result)))
+;; 		  (setq next-token-param (if (not (eq next-token :null)) (format " --starting-token %s" next-token)))
+;; 		  (setq has-next (not (null next-token-param)))
+;; 		  (setq l (+ l (length (gethash "Parameters" result)))))))
+;; 	(message "l: %s" l)))
+
+;; display values
 (defun aws-ssm--truncate (value limit)
   (let* ((break-line-pos (aws-ssm--break-line-pos value))
 		 (first-line (substring value 0 break-line-pos)))
@@ -91,27 +120,14 @@
 (defun aws-ssm--truncate-single-line? (value limit)
   (> (length value) limit))
 
-(defun aws-ssm--get-parameters-names-async (command)
-  (lambda ()
-	(defun execute-command ()
-	  (json-parse-string (shell-command-to-string command)))
+;; data serialization
+(defun aws-ssm--read-data (filename)
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (cl-assert (eq (point) (point-min)))
+    (read (current-buffer))))
 
-	(defun parse-describe-parameters-resp (command-result-json)
-	  (mapcar (lambda (x)
-				(gethash "Name" x))
-			  (gethash "Parameters" command-result-json)))
-	
-	(parse-describe-parameters-resp (execute-command))))
-
-(defun aws-ssm--get-parameters-values-async (command)
-  (lambda ()
-	(defun execute-command ()
-	  (json-parse-string (shell-command-to-string command)))
-
-	(defun parse-get-parameters-resp (command-result-json)
-	  (mapcar (lambda (x)
-				`(,(gethash "Name" x)
-				  ,(gethash "Value" x)))
-			  (gethash "Parameters" command-result-json)))
-    
-	(parse-get-parameters-resp (execute-command))))
+(defun aws-ssm--write-data (filename data)
+  (with-temp-file
+	  filename
+	(prin1 data (current-buffer))))
